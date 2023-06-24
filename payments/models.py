d0e262ff435c5bcfa07b42c2669c33e3aaa0ce82
likem3 <islike1221@gmail.com>
2023-06-24 13:15:41 +0800

    remove unneeded code

--- payments/models.py
+++ payments/models.py
@@ -1,6 +1,5 @@
 from django.db import models
-from txidck.models import BaseModel, ExtraBaseModel
-from users.models import Account, Wallet as AccountWallet
+from txidck.models import BaseModel
 
 class Wallet(BaseModel):
     STATUS_CHOICES = (
@@ -22,23 +21,3 @@ class Wallet(BaseModel):
     
     class Meta:
         db_table = 'payments_wallets'
-
-
-# class Recharge(ExtraBaseModel):
-#     STATUS_CHOICES = (
-#         ('pending', 'Pending'),
-#         ('completed', 'Completed'),
-#         ('failed', 'Failed'),
-#         ('cancelled', 'Cancelled'),
-#     )
-
-#     account_id = models.ForeignKey(to=Account, on_delete=models.CASCADE)
-#     wallet_id = models.ForeignKey(to=AccountWallet, on_delete=models.CASCADE, related_name='account_wallet')
-#     wallet_address = models.CharField(max_length=100)
-#     payment_wallet_id = models.ForeignKey(to=Wallet, on_delete=models.CASCADE)
-#     payment_wallet_address = models.CharField(max_length=100)
-#     amount = models.DecimalField(max_digits=16, decimal_places=6)
-#     blockchain = models.CharField(max_length=50)
-#     network = models.CharField(max_length=30)
-#     status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='pending')
-#     cancel_reason = models.TextField(null=True, blank=True)
