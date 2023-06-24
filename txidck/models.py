179a10d6c8d4e78fd55d29dfef7dd42f74c9a0fc
likem3 <islike1221@gmail.com>
2023-06-24 13:11:36 +0800

    add extra base model

--- txidck/models.py
+++ txidck/models.py
@@ -1,9 +1,21 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
 class BaseModel(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class ExtraBaseModel(BaseModel):
+    deleted_at = models.DateTimeField(null=True, blank=True)
+    created_by = models.ForeignKey(to=User, on_delete=models.CASCADE, related_name='created_by')
+    approved_by = models.ForeignKey(to=User, on_delete=models.CASCADE, null=True, related_name='approved_by')
+    cancelled_by = models.ForeignKey(to=User, on_delete=models.CASCADE, null=True, related_name='cancelled_by')
+    deleted_by = models.ForeignKey(to=User, on_delete=models.CASCADE, null=True, related_name='deleted_by')
 
     class Meta:
         abstract = True
--- txidck/settings.py
+++ txidck/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 
     'users',
     'payments',
+    'recharges',
 ]
 
 MIDDLEWARE = [
